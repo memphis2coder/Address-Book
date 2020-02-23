b68f01abe89f9a68fc94317e92cee5df2155e0a6
Antonio <Memphis2Coder@gmail.com>
2020-02-23 15:43:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// mask the number while typing... this formats the phone number input (111)-111-1111
+document.getElementById('phone').addEventListener('input', function (e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
+
